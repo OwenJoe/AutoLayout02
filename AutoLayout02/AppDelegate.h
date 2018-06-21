9883ea1f655b4865b4958a37a0ff994a51db741f
owen <zhuwb@hori-gz.com>
2018-06-21 18:44:52 +0800

    AutoLayout02练习

---
+++ AutoLayout02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout02
+//
+//  Created by owen on 2018/6/21.
+//  Copyright © 2018年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout02/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout02
+//
+//  Created by owen on 2018/6/21.
+//  Copyright © 2018年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
